f9c3cfd8c2de68b358f360dd0dfcb383ee9c99ce
Gustavo Insaurralde <insaurralde.ap@gmail.com>
2013-08-17 18:25:07 -0300

    added "list data" function

--- form_stuff.js
+++ form_stuff.js
@@ -11,5 +11,18 @@ $().ready(function(){
 	   food.save({value:formDetails}); //"INSERT"
 	   });				
  });
+
+
+//List persisted objects on indexed-db
+$().ready(function(){
 	
+//List persisted objects on indexed-db
+ function listData() {
+	Lawnchair({name : 'food', adapter:'indexed-db'}, function() {			//Note this "namespace" "food" is the same that when we save
+	  this.get("ad3bceba-3718-eb9a-f17e-53d76ef8bd30", function(config) {	//The long string is one I extracted from the browser´s indexedDB
+	    console.log(config);
+	  })
+	})
+ }
+});
 });
