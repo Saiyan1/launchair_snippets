577710770d1ee7eef9e71d9c644b7ad15652e9ce
Gustavo Insaurralde <insaurralde.ap@gmail.com>
2013-08-17 17:27:16 -0300

    Translate and format

--- form_stuff.js
+++ form_stuff.js
@@ -1,16 +1,15 @@
 //Import JQuery , launchair.js  and launchair "indexed db adapter" ...
 
-//Listening form submissions with
-
-		$().ready(function(){
-			
-			$( "form" ).submit(function( event ) {
-				event.preventDefault();
-				var JSONObject = $("form").serializeArray();	//Serializa el form a JSON
-				var db = Lawnchair({name : 'food', adapter:'indexed-db'}, function(food) {
-					var formDetails = JSONObject;				
-					food.save({value:formDetails}); //INSERT (Si se pasa el id como atributo "key", hace un update)
-				});				
-			});
-			
-		});
+//Listening form submissions with JQuery
+$().ready(function(){
+	
+ $( "form" ).submit(function( event ) {
+	event.preventDefault();
+	var JSONObject = $("form").serializeArray();	//Serialize form to JSON (http://api.jquery.com/serializeArray/)
+	var db = Lawnchair({name : 'food', adapter:'indexed-db'}, function(food) {
+	   var formDetails = JSONObject;				
+	   food.save({value:formDetails}); //"INSERT"
+	   });				
+ });
+	
+});
